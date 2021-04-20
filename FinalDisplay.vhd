e1b56d92047f8f2e14582852d34505f7432691d3
josephwang3 <josephxawang@gmail.com>
2021-04-20 17:45:57 -0500

    Added month and date

--- FinalDisplay.vhd
+++ FinalDisplay.vhd
@@ -32,25 +32,32 @@ use IEEE.NUMERIC_STD.ALL;
 entity FinalDisplay is
     Port ( --SEC_0 : in  STD_LOGIC_VECTOR(3 downto 0);	
 				-- clock signal
-				CLK : in STD_LOGIC;
+				BOARD_CLK : in STD_LOGIC;
 				
-				-- 4 digits on board				
-				AN1 : out  STD_LOGIC;
-				AN2 : out  STD_LOGIC;
-				AN3 : out  STD_LOGIC;
-				AN4 : out  STD_LOGIC;
-				AN5 : out STD_LOGIC;
-				AN6 : out STD_LOGIC;
+				-- 10 digits on board, controlled by common cathodes (common ground)
+				-- 6 for hour, minute, second
+				C_SEC1 : out  STD_LOGIC;
+				C_SEC2 : out  STD_LOGIC;
+				C_MIN1 : out  STD_LOGIC;
+				C_MIN2 : out  STD_LOGIC;
+				C_HOUR1 : out  STD_LOGIC;
+				C_HOUR2 : out  STD_LOGIC;
+				
+				-- 4 for month, day
+				C_DAY1 : out  STD_LOGIC;
+				C_DAY2 : out  STD_LOGIC;
+				C_MONTH1 : out  STD_LOGIC;
+				C_MONTH2 : out  STD_LOGIC;	
 	 
-				-- 7 outputs for 7 segment display
-           C_A : out  STD_LOGIC;
-           C_B : out  STD_LOGIC;
-           C_C : out  STD_LOGIC;
-           C_D : out  STD_LOGIC;
-           C_E : out  STD_LOGIC;
-           C_F : out  STD_LOGIC;
-           C_G : out  STD_LOGIC;
-			  DP : out STD_LOGIC;
+				-- 7 anode outputs for 7 segment display
+           AN_A : out  STD_LOGIC;
+           AN_B : out  STD_LOGIC;
+           AN_C : out  STD_LOGIC;
+           AN_D : out  STD_LOGIC;
+           AN_E : out  STD_LOGIC;
+           AN_F : out  STD_LOGIC;
+           AN_G : out  STD_LOGIC;
+			  AN_DP : out STD_LOGIC;
 			  
 			  -- LED 0 for clock 2 indicator
 			  LD0 : out STD_LOGIC;
@@ -59,19 +66,27 @@ entity FinalDisplay is
 			  RESET : in STD_LOGIC;
 			  
 			  -- clock 2 from 555 timer
-			  clk2 : in STD_LOGIC);
+			  clk2 : in STD_LOGIC;
+			  
+			  -- set switch
+			  	set : in STD_LOGIC;
+
+			  	-- set buttons
+				set_min : in STD_LOGIC;
+				set_hour : in STD_LOGIC;
+				set_day : in STD_LOGIC);
 end FinalDisplay;
 
 
 architecture Behavioral of FinalDisplay is
 	-- clock NUMBER
-	signal CLK_COUNTER : natural range 0 to 50000000 := 0;
+	signal CLK_COUNTER : natural range 0 to 50000 := 0;
 	
-	-- which AN port to trigger
-	signal COUNTER: natural range 0 to 5 := 0;
+	-- which CATHODES port to trigger
+	signal COUNTER: natural range 0 to 9 := 0;
 	
-	-- AN ports
-	signal AN : std_logic_vector(5 downto 0);
+	-- common cathode ports for multiplexing digits
+	signal CATHODES : std_logic_vector(9 downto 0);
 	
 	-- BCD of digits
 	signal HOUR2 : STD_LOGIC_VECTOR(3 downto 0) := "0000"; 
@@ -88,75 +103,100 @@ architecture Behavioral of FinalDisplay is
 	signal DISPLAY : std_logic_vector(6 downto 0);
 	
 
-signal hour24: std_logic_vector(0 downto 0);
+	signal hour24: std_logic_vector(0 downto 0);
 
-signal dot: std_logic := '1'; -- AM & PM
+	-- decimal point to indicate AM/PM
+	signal dot: std_logic := '0';
 
---signal hset: std_logic_vector(0 downto 0);
---signal mset: std_logic_vector(0 downto 0);
---signal sset: std_logic_vector(0 downto 0);
+	-- this one contains 8 bits for the switch statement, it is one 4 bit
+	-- number and another 4 bit number, not an 8 bit number
+	signal month : std_logic_vector(7 downto 0);
+	
+	signal month1 : std_logic_vector(3 downto 0) := "0000";
+	signal month2 : std_logic_vector(3 downto 0) := "0000";
+
+	signal day1 : std_logic_vector(3 downto 0) := "0000";
+	signal day2 : std_logic_vector(3 downto 0) := "0000";
+
+	-- extra variables to know when to increment the month
+	signal maxday1 : std_logic_vector(3 downto 0); 
+	signal maxday2 : std_logic_vector(3 downto 0);
 
 
 begin
 	
 	-- do stuff every 1 ms, dividing clock signal
-	Clock_Divider2 : process (CLK)
+	Clock_Divider2 : process (BOARD_CLK)
 		begin
-			if (rising_edge(CLK)) then
+			if (rising_edge(BOARD_CLK)) then
 				CLK_COUNTER <= CLK_COUNTER + 1;
 				-- every 8000 ticks, so every 1 ms for a 8 MHz clock
 				if (CLK_COUNTER >= 8000) then
 					CLK_COUNTER <= 0;
 					
-					-- increment COUNTER from 1 to 3
+					-- increment COUNTER from 0 to 9 for 10 displays
 					COUNTER <= COUNTER + 1;
-					if (COUNTER > 5) then
+					if (COUNTER > 9) then
 						COUNTER <= 0;
 					end if;					
 				end if;
 			end if;
 	end process;
 	
-	-- use COUNTER to modify which AN port is on
-	Change_AN : process (COUNTER)
+	-- use COUNTER to modify which CATHODES port is on
+	Change_Cathodes : process (COUNTER)
 	begin
 		case COUNTER is
-			when 0 => AN <= "000001";
-			when 1 => AN <= "000010";
-			when 2 => AN <= "000100";
-			when 3 => AN <= "001000";
-			when 4 => AN <= "010000";
-			when 5 => AN <= "100000";
-			when others => AN <= "000000"; -- if counter above 5, all off
+			when 0 => CATHODES <= "1111111110";
+			when 1 => CATHODES <= "1111111101";
+			when 2 => CATHODES <= "1111111011";
+			when 3 => CATHODES <= "1111110111";
+			when 4 => CATHODES <= "1111101111";
+			when 5 => CATHODES <= "1111011111";
+			when 6 => CATHODES <= "1110111111";
+			when 7 => CATHODES <= "1101111111";
+			when 8 => CATHODES <= "1011111111";
+			when 9 => CATHODES <= "0111111111";
+			when others => CATHODES <= "1111111111"; -- if counter above 9, all off
 		end case;
 		
-		-- turn AN ports on or off accordingly
-		-- @ change for hours
-		AN1 <= AN(0);
-		AN2 <= AN(1);
-		AN3 <= AN(2);
-		AN4 <= AN(3);
-		AN5 <= AN(4);
-		AN6 <= AN(5);
+		-- turn CATHODES ports on or off accordingly
+		C_SEC1 <= CATHODES(0);
+		C_SEC2 <= CATHODES(1);
+		C_MIN1 <= CATHODES(2);
+		C_MIN2 <= CATHODES(3);
+		C_HOUR1 <= CATHODES(4);
+		C_HOUR2 <= CATHODES(5);
+		C_DAY1 <= CATHODES(6);
+		C_DAY2 <= CATHODES(7);
+		C_MONTH1 <= CATHODES(8);
+		C_MONTH2 <= CATHODES(9);		
 	end process;
 	
 	-- display decimal place based on counter, active low
 	Display_DP : process (COUNTER)
 	begin
 		case COUNTER is
-			when 0 => DP <= dot;
-			when 1 => DP <= '1';
-			when 2 => DP <= '0';
-			when 3 => DP <= '1';
-			when 4 => DP <= '0';
-			when 5 => DP <= '1';
-			when others => DP <= '1'; -- decimal place off if counter above 5
+			when 0 => AN_DP <= dot;
+			when 1 => AN_DP <= '0';
+			when 2 => AN_DP <= '1';
+			when 3 => AN_DP <= '0';
+			when 4 => AN_DP <= '1';
+			when 5 => AN_DP <= '0';
+			when 6 => AN_DP <= '0';
+			when 7 => AN_DP <= '0';
+			when 8 => AN_DP <= '1';
+			when 9 => AN_DP <= '0';
+			when others => AN_DP <= '0'; -- decimal place off if counter above 9
 		end case;
 	end process;
 	
 	-- change number displayed based on counter
 	Change_Number : process (COUNTER)
 	begin
+		month1 <= month(3 downto 0);
+		month2 <= month(7 downto 4);
+	
 		case COUNTER is
 			when 0 => NUMBER <= SEC1;
 			when 1 => NUMBER <= SEC2;
@@ -164,6 +204,10 @@ begin
 			when 3 => NUMBER <= MIN2;
 			when 4 => NUMBER <= HOUR1;
 			when 5 => NUMBER <= HOUR2;
+			when 6 => NUMBER <= DAY1;
+			when 7 => NUMBER <= DAY2;
+			when 8 => NUMBER <= MONTH1;
+			when 9 => NUMBER <= MONTH2;
 			when others => NUMBER <= "1111"; -- invalid number if counter above 5
 		end case;
 	end process;
@@ -173,37 +217,37 @@ begin
 	begin
 		case NUMBER is
 				when "0000" =>
-					DISPLAY <= "0000001"; -- 0
+					DISPLAY <= "1111110"; -- 0
 				when "0001" =>
-					DISPLAY <= "1001111"; -- 1
+					DISPLAY <= "0110000"; -- 1
 				when "0010" =>
-					DISPLAY <= "0010010"; -- 2
+					DISPLAY <= "1101101"; -- 2
 				when "0011" =>
-					DISPLAY <= "0000110"; -- 3
+					DISPLAY <= "1111001"; -- 3
 				when "0100" =>
-					DISPLAY <= "1001100"; -- 4
+					DISPLAY <= "0110011"; -- 4
 				when "0101" =>
-					DISPLAY <= "0100100"; -- 5
+					DISPLAY <= "1011011"; -- 5
 				when "0110" =>
-					DISPLAY <= "0100000"; -- 6
+					DISPLAY <= "1011111"; -- 6
 				when "0111" =>
-					DISPLAY <= "0001111"; -- 7
+					DISPLAY <= "1110000"; -- 7
 				when "1000" =>
-					DISPLAY <= "0000000"; -- 8
+					DISPLAY <= "1111111"; -- 8
 				when "1001" =>
-					DISPLAY <= "0000100"; -- 9
+					DISPLAY <= "1111011"; -- 9
 				when others =>
-					DISPLAY <= "1111111"; -- blank when not a digit
+					DISPLAY <= "0000000"; -- blank when not a digit
 		end case;
 		
 		-- send information to board
-		C_A <= DISPLAY(6);
-		C_B <= DISPLAY(5);
-		C_C <= DISPLAY(4);
-		C_D <= DISPLAY(3);
-		C_E <= DISPLAY(2);
-		C_F <= DISPLAY(1);
-		C_G <= DISPLAY(0);
+		AN_A <= DISPLAY(6);
+		AN_B <= DISPLAY(5);
+		AN_C <= DISPLAY(4);
+		AN_D <= DISPLAY(3);
+		AN_E <= DISPLAY(2);
+		AN_F <= DISPLAY(1);
+		AN_G <= DISPLAY(0);
 		
 		
 	end process;
@@ -212,7 +256,7 @@ begin
 	divide: process(clk2)
 	begin
 		if(rising_edge(clk2)) then
-		
+			
 			if(sec1 = "1001") then 
 				sec1 <= "0000";
 				
@@ -233,22 +277,98 @@ begin
 								elsif (hour1 = "0011" AND hour2 = "0010") then
 									hour1 <= "0000";
 									hour2 <= "0000";
+
+									case month is 
+										--january
+										when "00000001" => 
+											maxday1 <= "0001";
+											maxday2 <= "0011";
+										--february
+										when "00000010" => 
+											maxday1 <= "1000";
+											maxday2 <= "0010";
+										--march
+										when "00000011" =>
+											maxday1 <= "0001";
+											maxday2 <= "0011";
+										--april
+										when "00000100" =>
+											maxday1 <= "0000";
+											maxday2 <= "0011";
+										--may
+										when "00000101" =>
+											maxday1 <= "0001";
+											maxday2 <= "0011";
+										--june
+										when "00000110" =>
+											maxday1 <= "0000";
+											maxday2 <= "0011";
+										--july
+										when "00000111" =>
+											maxday1 <= "0001";
+											maxday2 <= "0011";
+										--august
+										when "00001000" =>
+											maxday1 <= "0001";
+											maxday2 <= "0011";
+										--september
+										when "00001001" =>
+											maxday1 <= "0000";
+											maxday2 <= "0011";
+										--october
+										when "00010000" =>
+											maxday1 <= "0001";
+											maxday2 <= "0011";
+										--november
+										when "00010001" =>
+											maxday1 <= "0000";
+											maxday2 <= "0011";
+										--december
+										when "00010010" =>
+											maxday1 <= "0001";
+											maxday2 <= "0011";
+										when others =>
+											maxday1 <= "0000";
+											maxday2 <= "0000";
+									end case;
+
+									if(day1 = maxday1 AND day2 = maxday2) then
+										day1 <= "0001";
+										day2 <= "0000";
+										if(month < "00001010") then 
+											month <= std_logic_vector(to_unsigned(to_integer(unsigned( month )) +1, 8));
+										elsif(month = "00001010") then 
+											month <= "00010000";
+										elsif(month = "00010000") then 
+											month <= "00010001";
+										elsif(month = "00010001") then 
+											month <= "00010010"; 
+										elsif(month = "00010010") then 
+											month <= "00000000"; 		
+										end if;
+									else
+										if(day1 = "1001") then
+											day1 <= "0000";
+											day2 <= std_logic_vector(to_unsigned(to_integer(unsigned( day2 )) +1, 4));
+										else
+											day1 <= std_logic_vector(to_unsigned(to_integer(unsigned( day1 )) +1, 4));
+										end if;
+									end if;
+
 								end if;
-							
-							-- 12 hour time
+								
 							else
-								-- for normal increments, when hour1 is 9, set it to 0 and increment hour2
+							--not 24
 								if(hour1 = "1001" AND (hour2 = "0000")) then
 									hour1 <= "0000";
 									hour2 <= std_logic_vector(to_unsigned(to_integer(unsigned( hour2 )) +1, 4));
-								-- when hit 12, turn PM indicator on, unless it is on, then turn it off
 								elsif (hour1 = "0010" AND hour2 = "0001") then
 									hour1 <= "0001";
 									hour2 <= "0000";
-										if (dot = '1')then
-											dot <= '0';
-										else
+										if (dot = '0')then
 											dot <= '1';
+										else
+											dot <= '0';
 										end if;
 										
 								end if;
@@ -275,8 +395,6 @@ begin
 			
 		end if;
 		
-		
-		-- reset clock if reset button, button 0, is pressed
 		if(RESET = '0') then
 			sec1 <= "0000";
 			sec2 <= "0000";
@@ -284,12 +402,15 @@ begin
 			min2 <= "0000";
 			hour1 <= "0000";
 			hour2 <= "0000";
-			dot <= '0';
+			month <= "00000000";
+			day1 <= "0000";
+			day2 <= "0000";
 		end if;
 		
-	end process;
+	end process divide;
 	
 	-- use LED 0 as output for clock
 	LD0 <= clk2;
+
 
 end Behavioral;
